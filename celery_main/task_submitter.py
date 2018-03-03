9f9dbb842399e010991e9e6274d3347ed2126076
rana <anuragrana31189@gmail.com>
2018-03-03 18:03:22 +0530

    updated the version of docker-compose

--- celery_main/task_submitter.py
+++ celery_main/task_submitter.py
@@ -4,4 +4,4 @@ from .task_receiver import longtime_add
 if __name__ == '__main__':
     for i in range(10):
         result = longtime_add.delay(i)
-        print('task submitted' + str(i))
+        print('task submitted ' + str(i))
